acdea92b89bb94fc90611494ea4b1e9002d40051
daniel skapunk <daniel.skapunk@gmail.com>
2023-11-29 22:05:48 -0600

    updated project structure

---
+++ config.ts
@@ -0,0 +1,58 @@
+// 1. Import utilities from `astro:content`
+
+import { defineCollection, z } from 'astro:content'
+
+// 2. Define your collection(s)
+const acroCollection = defineCollection({
+	type: 'content', // v2.5.0 and later
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			aka: z.array(z.string()),
+			level: z.enum(['easy', 'medium', 'hard']),
+			image: image().refine((img) => img.width >= 600, {
+				message: 'Cover image must be at least 1080 pixels wide!'
+			}),
+			video: z.optional(z.string()),
+			to: z.array(
+				z.object({
+					pose: z.string(),
+					slug: z.string(),
+					video: z.string(),
+					canGoBack: z.boolean()
+				})
+			),
+			tags: z.array(z.string()),
+			numPeople: z.enum(['two', 'three', 'more'])
+		})
+})
+
+const seqCollection = defineCollection({
+	type: 'content', // v2.5.0 and later
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			aka: z.array(z.string()),
+			level: z.enum(['easy', 'medium', 'hard']),
+			image: image().refine((img) => img.width >= 600, {
+				message: 'Cover image must be at least 1080 pixels wide!'
+			}),
+			video: z.optional(z.string()),
+			// to: z.array(
+			// 	z.object({
+			// 		pose: z.string(),
+			// 		video: z.string(),
+			// 		canGoBack: z.boolean()
+			// 	})
+			// ),
+			tags: z.array(z.string()),
+			numPeople: z.enum(['two', 'three', 'more'])
+		})
+})
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+	acro: acroCollection,
+	seq: seqCollection
+}
---
+++ seq/ninja-star.md
@@ -0,0 +1,35 @@
+---
+name: Ninja Star
+aka: []
+level: easy
+image: './images/acroyoga-bat.jpeg'
+video: 'https://youtu.be/WUtVJ46LT-M?t=232'
+tags: []
+numPeople: two
+---
+
+## Descripción
+
+[Inside Side Star](/acro/inside-side-star) --> [Reverse Bird](/acro/reverse-bird) --> [Inside Side Star](/acro/inside-side-star) --> [Bat](/acro/bat) -->
+
+## Drills
+
+<!-- [super dave video](https://www.youtube.com/watch?v=qa5iG6k3W0s) -->
+
+<!-- :::section{.variantes}
+
+## Variantes
+
+- ![Bow Pose](./images/acro-back-bird-bow.png)
+  _Bow pose_
+
+- ![Dancers pose](./images/acro-back-bird-dancers.png)
+  _Dancers pose_
+
+- ![Back Plank](./images/acro-back-bird-back-plank.png)
+  _Back plank_
+
+- ![Boat Pose](./images/acro-back-bird-boat.png)
+  _Boat pose_
+
+::: -->
